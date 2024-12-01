47630ca7d1117459f30275b2225a6748557b6a7b
Youssef Ali Saber <ya1654@fayoum.edu.eg>
2024-12-01 22:01:49 +0200

    lead crud operations using ado.net

---
+++ ADO.NET_CRUD_MVC/Controllers/LeadsController.cs
@@ -0,0 +1,78 @@
+﻿using ADO.NET_CRUD_MVC.Data;
+using ADO.NET_CRUD_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ADO.NET_CRUD_MVC.Controllers
+{
+    public class LeadsController(ILeadRepository leadsRepo) : Controller
+    {
+        public IActionResult Index()
+        {
+            var leads = leadsRepo.GetLeads();
+            return View(leads);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Lead lead)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(lead);
+            }
+
+            var check = leadsRepo.AddLead(lead);
+
+            if (check)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return View(lead);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var lead = leadsRepo.GetLead(id);
+            return View(lead);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var lead = leadsRepo.GetLead(id);
+            return View(lead);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(int id, Lead lead)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(lead);
+            }
+            var check = leadsRepo.UpdateLead(id, lead);
+            if (check)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(lead);
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            var check = leadsRepo.DeleteLead(id);
+            if (check)
+            {
+                return Json(new { success = true, message = "Lead deleted successfully." });
+            }
+            return Json(new { success = false, message = "Error deleting lead." });
+        }
+
+
+    }
+}
---
+++ ADO.NET_CRUD_MVC/Factory/SqlConnectionFactory.cs
@@ -0,0 +1,11 @@
+﻿using System.Data.SqlClient;
+
+namespace ADO.NET_CRUD_MVC.Factory;
+
+public class SqlConnectionFactory(string connectionString)
+{
+    public SqlConnection Create()
+    {
+        return new SqlConnection(connectionString);
+    }
+}
